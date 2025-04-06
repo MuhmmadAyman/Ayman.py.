1aac5c6b39b744d8f050666d78d4ea81f12e6b87
MuhmmadAyman <muhammad24ai@drttit.edu.in>
2025-04-06 08:20:38 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+dic={
+     "Ayman":100,
+     "Aiman":99,
+     "grbbgreji":22,
+     "iiuughreb":233,
+
+}
+n=input("Enter the name of the student:")
+if n in dic:
+    print(dic[n])
+else:
+    print("Not found")
---
+++ list.py
@@ -0,0 +1,6 @@
+lis=[1,2,3,1,2,3,4,5,6,2]
+n=lis[:5]
+print("originaal list",lis)
+print("extraxted lis",n)
+print("reversed list",n[::-1])
+
